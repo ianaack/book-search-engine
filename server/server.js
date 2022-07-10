9a448c87114cfa7f23d322de01bb1c6f9bf3abfb
ianaack <ianaack@gmail.com>
2022-07-09 22:47:11 -0400

    Implement Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,40 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+// Implement the Apollo Server
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
-const app = express();
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: authMiddleware,
+	});
+	await server.start();
+	server.applyMiddleware({ app });
+	console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once("open", () => {
+	app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
