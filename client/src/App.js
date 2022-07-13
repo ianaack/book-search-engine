0f6d071532da0ed32238d7c9877e92b370ed0e3d
ianaack <ianaack@gmail.com>
2022-07-12 22:51:21 -0400

    updated authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // import Apollo Client library components
 import {
 	ApolloClient,
@@ -7,6 +8,7 @@ import {
 	ApolloProvider,
 	createHttpLink,
 } from "@apollo/client";
+
 import { setContext } from "@apollo/client/link/context";
 
 import SearchBooks from "./pages/SearchBooks";
@@ -38,11 +40,11 @@ function App() {
 			<Router>
 				<>
 					<Navbar />
-					<Switch>
+					<Routes>
 						<Route exact path="/" component={SearchBooks} />
 						<Route exact path="/saved" component={SavedBooks} />
 						<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-					</Switch>
+					</Routes>
 				</>
 			</Router>
 		</ApolloProvider>
